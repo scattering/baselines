9ef2ad4fea2d0316d1d251e6913256ef51da6525
Kate Meuse <kmm522@cornell.edu>
2020-12-23 14:32:03 -0500

    correct indentation

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-	import re
-	from setuptools import setup, find_packages
-	import sys
-	import platform
+import re
+from setuptools import setup, find_packages
+import sys
+import platform
 
-	if sys.version_info.major != 3:
-	    print('This Python is only compatible with Python 3, but you are running '
-		  'Python {}. The installation will likely fail.'.format(sys.version_info.major))
+if sys.version_info.major != 3:
+	print('This Python is only compatible with Python 3, but you are running '
+	  'Python {}. The installation will likely fail.'.format(sys.version_info.major))
 
 
 	extras = {
